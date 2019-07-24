979e08f7824424f685c555521742e40660ef301a
Zoeckler <lzoeckler@path.org>
2019-07-24 13:40:27 -0700

    prepping for removal

--- Mosaic/make_table.py
+++ Mosaic/make_table.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 import numpy as np
 import pandas as pd
 # import calc_bright_cells function
@@ -41,31 +40,3 @@ def model_table(input_dir, fsc_filt, ssc_filt,
                                                         'Std Dev FL1', 'Percent Bright Cells'])
     return zygo_results
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-id', '--input_dir', type=str,
-                        help='Directory that contains FCS files')
-    parser.add_argument('-ff', '--fsc_filt', type=list,
-                        default=[.4, .95],
-                        help='Minimum and maximum FSC channel values')
-    parser.add_argument('-sf', '-ssc_filt', type=list,
-                        default=[.05, .6],
-                        help='Minimum and maximum SSC channel values')
-    parser.add_argument('-fl1', type=str,
-                        default='FL1-A',
-                        help='Name of the FL1 channel')
-    parser.add_argument('-fsc', type=str,
-                        default='FSC-H',
-                        help='Name of the FSC channel')
-    parser.add_argument('-ssc', type=str,
-                        default='SSC-H',
-                        help='Name of the SSC channel')
-    parser.add_argument('-a', '--amplification', action='store_true',
-                        help='Whether or not to run with amplification (log)')
-    parser.add_argument('-mps', '--min_peak_size', type=float,
-                        default=.003,
-                        help='Minimum peak height to keep')
-    args = parser.parse_args()
-    model_table(input_dir=args.id, fsc_filt=args.ff, ssc_filt=args.sf, fl1=args.fl1,
-                fsc=args.fsc, ssc=args.ssc, amplification=args.a, min_peak_size=args.mps)
--- Mosaic/mosaic_master.py
+++ Mosaic/mosaic_master.py
@@ -1,10 +1,50 @@
+import os
+import argparse
 from FlowCytometryTools import FCMeasurement
-from Mosaic.make_table import model_table
-from Mosaic.calc_bright_cells import prep_fcs, calc_bright_cells
+from Mosaic.mosaic_functions import model_table, prep_fcs, calc_bright_cells
 
-test = FCMeasurement(ID='', datafile='{}/1.fcs'.format(fcs_dir))
-channels = test.channels
 
-table = model_table()
+def main(input_dir, fsc_filt, ssc_filt,
+         fl1, fsc, ssc, amplification=False,
+         min_peak_size=0.003):
+    test = FCMeasurement(ID='', datafile='{}/1.fcs'.format(input_dir))
+    channels = test.channels
 
-data = prep_fcs(fcs)
+    table = model_table(input_dir, fsc_filt, ssc_filt,
+                        fl1, fsc, ssc, amplification,
+                        min_peak_size)
+
+    file_path = os.listdir(input_dir)
+    data = prep_fcs(file_path, fsc_filt, ssc_filt,
+                    fl1, fsc, ssc, amplification)
+
+    bc_percent, mean_fitc, median_fitc, sd_fitc = calc_bright_cells(data, fl1, min_peak_size)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-id', '--input_dir', type=str,
+                        help='Directory that contains FCS files')
+    parser.add_argument('-ff', '--fsc_filt', type=list,
+                        default=[.4, .95],
+                        help='Minimum and maximum FSC channel values')
+    parser.add_argument('-sf', '-ssc_filt', type=list,
+                        default=[.05, .6],
+                        help='Minimum and maximum SSC channel values')
+    parser.add_argument('-fl1', type=str,
+                        default='FL1-A',
+                        help='Name of the FL1 channel')
+    parser.add_argument('-fsc', type=str,
+                        default='FSC-H',
+                        help='Name of the FSC channel')
+    parser.add_argument('-ssc', type=str,
+                        default='SSC-H',
+                        help='Name of the SSC channel')
+    parser.add_argument('-a', '--amplification', action='store_true',
+                        help='Whether or not to run with amplification (log)')
+    parser.add_argument('-mps', '--min_peak_size', type=float,
+                        default=.003,
+                        help='Minimum peak height to keep')
+    args = parser.parse_args()
+    main(input_dir=args.id, fsc_filt=args.ff, ssc_filt=args.sf, fl1=args.fl1,
+         fsc=args.fsc, ssc=args.ssc, amplification=args.a, min_peak_size=args.mps)
